cfdef70ab2ebf59edc40df5bded72d264c6249c1
hyttttt <bianca20011228@gmail.com>
2023-07-22 15:42:34 +0800

    test external component

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ReportView from '../views/ReportView.vue'
 
+import TestPage from '../views/TestPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
